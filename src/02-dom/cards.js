cd8e8b18eebb7db862b83a759b08e99d3e6a0002
Kelly H <kellyaccnts@gmail.com>
2019-10-16 09:54:41 -0700

    Comp 100 & DOM experiments WIP

--- src/02-dom/cards.js
+++ src/02-dom/cards.js
@@ -1,19 +1,40 @@
+let body = document.querySelector("body").addEventListener("click", (e) =>{
+    let index = e.target.parentElement.getAttribute('data-index');
+    if (e.target.className == "cardAddBeforeButton") {
+        console.log(index)
+    }
+    if (e.target.className == "cardAddAfterButton") {
+    }
+    if (e.target.className == "cardDeleteButton") {
+    }
+});
+
 const cards = {
     cardAmount: 0,
+    cardCSSID:"card",
+    addBeforeButtonClass: "addBeforeButton",
+    addAfterButtonClass: "addAfterButton",
+    deleteButtonClass: "deleteButton",
 
     addCard:()=>{
-        return "nothing"
-    },
-    addCardBefore:(card)=>{
-        return {}
-    },
-    addCardAfter:(card)=>{
-        return {}
+        cardAmount++;
+        let toAdd = constructCard(cardAmount);
+
+        return "nothing";
     },
     deleteCard:(card)=>{
         // Return the card we're deleting
-        return {}
+        return card;
+    },
+    constructCard:(cardNum)=>{
+        return `<div id=${cardCSSID}>
+                    <p>Card ${cardNum}</p>
+                    <button class=${addBeforeButtonClass}>Add Before</button>
+                    <button class=${addAfterButtonClass}>Add After</button>
+                    <button class=${deleteButtonClass}>Delete</button>
+                </div>`;
     }
+
 };
 
 export default cards;
