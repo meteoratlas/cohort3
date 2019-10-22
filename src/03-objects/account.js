ffbda3ae2a2d4c390cbcc31d191bf40935053dea
Kelly H <kellyaccnts@gmail.com>
2019-10-21 23:16:32 -0700

    Comp 130 init and stubs

---
+++ src/03-objects/account.js
@@ -0,0 +1,14 @@
+class Account{
+    constructor(accountName, initBalance){
+
+    }
+    deposit(toDeposit){
+        return 0;
+    }
+    withdraw(toWithdraw){
+        return 0;
+    }
+    balance(){
+        return 0;
+    }
+}
