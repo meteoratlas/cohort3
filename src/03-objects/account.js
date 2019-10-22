64ed4fd8d0edff192fed2cd8044c67e318ab633d
Kelly H <kellyaccnts@gmail.com>
2019-10-22 10:47:24 -0700

    Comp 130 account implementation

--- src/03-objects/account.js
+++ src/03-objects/account.js
@@ -1,14 +1,33 @@
-class Account{
-    constructor(accountName, initBalance){
-
+class Account {
+    constructor(accountName, initBalance) {
+        this.name = String(accountName);
+        if (initBalance < 0) {
+            initBalance = 0;
+        } else {
+            this.funds = this.roundToFixed2(initBalance);
+        }
     }
-    deposit(toDeposit){
-        return 0;
+    deposit(toDeposit) {
+        if (toDeposit <= 0) {
+            return "Please enter a positive dollar amount.";
+        }
+        this.funds += this.roundToFixed2(toDeposit);
     }
-    withdraw(toWithdraw){
-        return 0;
+    withdraw(toWithdraw) {
+        if (toWithdraw <= 0) {
+            return "Please enter a positive dollar amount.";
+        }
+        if (toWithdraw > this.balance()) {
+            return "Your account does not contain the requested funds.";
+        }
+        this.funds -= this.roundToFixed2(toWithdraw);
     }
-    balance(){
-        return 0;
+    balance() {
+        return this.funds;
+    }
+    roundToFixed2(num) {
+        return Math.round(num * 100) / 100;
     }
 }
+
+export default Account;
---
+++ src/03-objects/tests/account.test.js
@@ -0,0 +1,43 @@
+import Account from '../account';
+
+test("Test that the account object was initialized correctly.", () => {
+    let acc = new Account("checkingAccount", 25)
+    expect(acc).toBeInstanceOf(Account);
+    expect(acc.funds).toBe(25);
+    expect(acc.name).toBe("checkingAccount");
+});
+
+test("Test rounding function", () => {
+    let acc = new Account("checkingAccount", 25)
+    expect(acc.roundToFixed2(54.32925)).toBe(54.33);
+    expect(acc.roundToFixed2(32.65323)).toBe(32.65);
+    expect(acc.roundToFixed2(15.99999)).toBe(16.00);
+});
+
+test("Test deposit function", () => {
+    let acc = new Account("checkingAccount", 0);
+    acc.deposit(20);
+    expect(acc.balance()).toBe(20);
+    acc.deposit(50.44);
+    expect(acc.balance()).toBe(70.44);
+    expect(acc.deposit(-1.44)).toBe("Please enter a positive dollar amount.");
+});
+
+test("Test withdraw function", () => {
+    let acc = new Account("checkingAccount", 50);
+    acc.withdraw(20);
+    expect(acc.balance()).toBe(30);
+    acc.withdraw(10.50);
+    expect(acc.balance()).toBe(19.50);
+    expect(acc.withdraw(-5.00)).toBe("Please enter a positive dollar amount.");
+    expect(acc.withdraw(50000.00)).toBe("Your account does not contain the requested funds.");
+});
+
+test("Test balance function", () => {
+    let acc = new Account("checkingAccount", 25.34)
+    expect(acc.balance()).toBe(25.34);
+    acc.withdraw(2.20);
+    expect(acc.balance()).toBe(23.14);
+    acc.deposit(100.00);
+    expect(acc.balance()).toBe(123.14);
+});
