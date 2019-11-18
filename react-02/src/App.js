0b59a7c974a4ea1da8708845398b710354f6e6a2
Kelly H <kellyaccnts@gmail.com>
2019-11-18 13:33:23 -0700

    Visual improvements for main App

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
     };
     render() {
         return (
-            <div className="App">
+            <div className="App .wobble-hor-bottom">
                 <Icon
                     pic="house.svg"
                     name="home"
