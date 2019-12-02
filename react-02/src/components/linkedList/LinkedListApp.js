49463fb7f0766974787a4c371b42593e431a6c4a
Kelly H <kellyaccnts@gmail.com>
2019-12-01 20:17:32 -0700

    implemented useInputState hook, added to linked list form

--- react-02/src/components/linkedList/LinkedListApp.js
+++ react-02/src/components/linkedList/LinkedListApp.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import LinkedListControls from "./ListedListControls";
+import { DoublyLinkedList } from "./model/DoublyLinkedList";
 
 const LinkedListApp = () => {
+    const [list, setList] = useState(new DoublyLinkedList());
+
     return (
         <div id="ll-app">
             <h2>Linked List</h2>
--- react-02/src/components/linkedList/ListedListControls.js
+++ react-02/src/components/linkedList/ListedListControls.js
@@ -1,6 +1,13 @@
 import React from "react";
+import useFormState from "./hooks/useInputState";
 
 const LinkedListControls = () => {
+    const [subject, setSubject, resetSubject] = useFormState("");
+    const [amount, setAmount, resetAmount] = useFormState("");
+    const handleSubmit = () => {
+        resetAmount();
+        resetSubject();
+    };
     return (
         <div>
             <div id="ll-controls-nav">
@@ -11,10 +18,18 @@ const LinkedListControls = () => {
             </div>
             <br />
             <div id="ll-controls-mod">
-                <input name="subject"></input>
-                <input name="amount"></input>
+                <input
+                    name="subject"
+                    value={subject}
+                    onChange={setSubject}
+                ></input>
+                <input
+                    name="amount"
+                    value={amount}
+                    onChange={setAmount}
+                ></input>
                 <br />
-                <button>Insert New Node Here</button>
+                <button onClick={handleSubmit}>Insert New Node Here</button>
                 <button>Delete Current Node</button>
             </div>
         </div>
