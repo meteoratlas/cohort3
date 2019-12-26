fafa5fa76ad498f3f26b3fe471cc314a6f593f9b
Kelly H <kellyaccnts@gmail.com>
2019-12-26 16:11:25 -0700

    finished python syntax testing

--- src/python/test_syntax.py
+++ src/python/test_syntax.py
@@ -13,6 +13,7 @@ class TestSyntax(unittest.TestCase):
         self.assertEqual([1,2,3,5], syntax.var_type("lIsT"))
         self.assertEqual((1,2,3), syntax.var_type("tuple"))
         self.assertEqual(None, syntax.var_type("anything else"))
+        self.assertEqual(None, syntax.var_type(None))
 
     def test_if_else(self):
         self.assertEqual("num is one", syntax.if_else(1))
@@ -31,6 +32,40 @@ class TestSyntax(unittest.TestCase):
         self.assertEqual([1,2,[1,2]], syntax.add_back([1,2], [1,2]))
         self.assertEqual([None], syntax.add_back([], None))
         self.assertEqual(12, syntax.add_back(12, "string"))
+
+    def test_update_values(self):
+        self.assertEqual([2,4,6,8], syntax.update_values(lambda x: x * 2, [1,2,3,4]))
+        self.assertEqual([1.5,2,2.5], syntax.update_values(lambda x: x / 2 + 1, [1,2,3]))
+        self.assertEqual([2,4], syntax.update_values(1, [2,4]))
+        self.assertEqual("string", syntax.update_values(lambda x: x * 2, "string"))
+
+    def test_for_loop(self):
+        self.assertEqual("12345", syntax.for_loop(5))
+        self.assertEqual("12", syntax.for_loop(2))
+        self.assertEqual(None, syntax.for_loop("string"))
+        self.assertEqual(None, syntax.for_loop(-5))
+
+    def test_for_in(self):
+        self.assertEqual("1234", syntax.for_in([1,2,3,4]))
+        self.assertEqual("abcd", syntax.for_in(["a","b","c","d"]))
+        self.assertEqual("string", syntax.for_in("string"))
+        self.assertEqual(-5, syntax.for_in(-5))
+
+    def test_while_loop(self):
+        self.assertEqual("12345", syntax.while_loop(5))
+        self.assertEqual("12", syntax.while_loop(2))
+        self.assertEqual(None, syntax.while_loop("string"))
+        self.assertEqual(None, syntax.while_loop(-5))
+
+    def test_get_by_key(self):
+        example = {"name":"Jen", "job":"typist", "salary":24}
+        self.assertEqual("Jen", syntax.get_by_key("name", example))
+        self.assertEqual("typist", syntax.get_by_key("job", example))
+        self.assertEqual(24, syntax.get_by_key("salary", example))
+        self.assertEqual("Key not in dictionary", syntax.get_by_key("age", example))
+        self.assertEqual("Key not in dictionary", syntax.get_by_key(24, example))
+        self.assertEqual("Object passed is not dictionary", syntax.get_by_key("job", ["name","Jen", "job","typist", "salary",24]))
+        self.assertEqual("Object passed is not dictionary", syntax.get_by_key("job", 2))
 
 if __name__ == '__main__':
     unittest.main()
