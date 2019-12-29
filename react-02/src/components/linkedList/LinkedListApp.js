f93249cdddad220e38eccda9ba1bc9846478fe6b
Kelly H <kellyaccnts@gmail.com>
2019-12-28 22:17:36 -0700

    ll paintings render code

--- react-02/src/components/linkedList/LinkedListApp.js
+++ react-02/src/components/linkedList/LinkedListApp.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import LinkedListControls from "./ListedListControls";
+import LinkedListPaintingControls from "./LinkedListPaintingControls";
 import { DoublyLinkedList } from "./model/DoublyLinkedList";
 import useLinkedList from "../hooks/useLinkedList";
+import { PLL } from "./model/PLL";
 import { ThemeContextConsumer } from "../../ThemeContextProvider";
 import data from "../../paintings/paintings.json";
 
 const LinkedListApp = () => {
+    const displayPaintings = true;
     const [
         list,
         currentNode,
@@ -17,6 +20,7 @@ const LinkedListApp = () => {
         lastNode
     ] = useLinkedList(new DoublyLinkedList("Apples", 2));
     const [paintings, setPaintings] = useState([]);
+    const [pll, setpll] = useState(new PLL());
     //
     const renderNodes = () => {
         return list.map(x => (
@@ -29,17 +33,6 @@ const LinkedListApp = () => {
             </React.Fragment>
         ));
     };
-    // const renderPaintingNodes = () => {
-    //     return list.map(x => (
-    //         <React.Fragment key={x.id}>
-    //             <div className="ll-node-vis">
-    //                 <p>{x.subject ? x.subject : "[Empty]"}</p>
-    //                 <p>{x.amount ? x.amount : "[Empty]"}</p>
-    //             </div>
-    //             <h4 className="ll-arrow">⮂</h4>
-    //         </React.Fragment>
-    //     ));
-    // };
     const loadPaintings = () => {
         const arr = [];
         data.paintings.map(p => {
@@ -55,11 +48,13 @@ const LinkedListApp = () => {
         const newArr = paintings.splice(rand, 1);
         setPaintings(paintings);
         // return newArr[0];
-        return testPaint(newArr[0]);
+        // return testPaint(newArr[0]);
+        let { title, artist, year, url, imgur } = newArr[0];
+        pll.insert(title, artist, year, imgur);
     };
-    const testPaint = obj => {
-        return (
-            <React.Fragment key="34534378900053">
+    const renderPaintings = () => {
+        return pll.map(obj => (
+            <React.Fragment key="obj.url">
                 <div className="ll-node-vis">
                     <img src={obj.imgur} alt="" height="150" />
                     <p>{obj.title}</p>
@@ -67,7 +62,7 @@ const LinkedListApp = () => {
                 </div>
                 <h4 className="ll-arrow">⮂</h4>
             </React.Fragment>
-        );
+        ));
     };
     useEffect(() => {
         loadPaintings();
@@ -77,15 +72,26 @@ const LinkedListApp = () => {
             {theme => (
                 <div id="ll-app" style={{ fontSize: theme.fontSize }}>
                     <h2>Linked List</h2>
-                    <LinkedListControls
-                        first={firstNode}
-                        //first={() => popPainting()}
-                        next={nextNode}
-                        prev={prevNode}
-                        last={lastNode}
-                        insert={insert}
-                        delete={deleteNode}
-                    />
+                    {displayPaintings ? (
+                        <LinkedListPaintingControls
+                            first={firstNode}
+                            next={nextNode}
+                            prev={prevNode}
+                            last={lastNode}
+                            addPainting={popPainting}
+                            delete={deleteNode}
+                        />
+                    ) : (
+                        <LinkedListControls
+                            first={firstNode}
+                            next={nextNode}
+                            prev={prevNode}
+                            last={lastNode}
+                            insert={insert}
+                            delete={deleteNode}
+                        />
+                    )}
+
                     <p>
                         Subject:{" "}
                         {currentNode ? currentNode.subject : "List is empty"}
@@ -94,7 +100,9 @@ const LinkedListApp = () => {
                         Amount:{" "}
                         {currentNode ? currentNode.amount : "List is empty"}
                     </p>
-                    <div id="ll-display">{renderNodes()}</div>
+                    <div id="ll-display">
+                        {displayPaintings ? renderPaintings() : renderNodes()}
+                    </div>
                 </div>
             )}
         </ThemeContextConsumer>
