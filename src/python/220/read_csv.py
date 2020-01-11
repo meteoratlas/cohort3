709e092579493fab6136f939acecb6630a038e7f
Kelly H <kellyaccnts@gmail.com>
2020-01-10 17:47:22 -0700

    improved report formatting & printing

--- src/python/220/read_csv.py
+++ src/python/220/read_csv.py
@@ -16,23 +16,24 @@ def read_file(csv_file, columns):
                     lines[row[col]] = 1
     return result, lines
 
-def print_report(data):
-    report =  "===========================================================\n"
-    report += "================= RESIDENCY CENSUS REPORT =================\n"
-    report += "===========================================================\n"
+def print_report(data, width, gutter = 5):
+    col_width = int(width / len(data) - gutter)
+    report =  "=" * width + "\n"
+    report += " RESIDENCY CENSUS REPORT ".center(width, "=") + "\n"
+    report += "=" * width + "\n"
 
     for i in data[0]:
         report += "\n"
-        report += "-----------------------------------------------------------\n"
-        report += f"----------------- Residency by {i} ---------------------\n"
-        report += "-----------------------------------------------------------\n"
-        report += f"{i}".ljust(25) + "Population".ljust(25) + "Lines\n\n"
+        report += "-" * width + "\n"
+        report += f" Residency by {i.title()} ".center(width, "-") + "\n"
+        report += "-" * width + "\n"
+        report += f"{i.title()}".ljust(col_width) + "Population".ljust(col_width) + "Lines\n\n"
 
         for (k, v) in data[0][i].items():
-            report += (f"{str(k).ljust(25)}{str(v).ljust(25)}{data[1][k]}\n")
+            report += f"{str(k).title().ljust(col_width)}{str(v).ljust(col_width)}{data[1][k]}\n"
             
     with open("Residency Report.txt", "w") as txt:
         txt.write(report)
 
 data = read_file("Census_by_Community_2018.csv", ["CLASS", "SECTOR"])
-print_report(data)
+print_report(data, 60) # report width of 60 characters
