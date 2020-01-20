cb168d1b1716ec529c7a0432bead2b69e0482fbb
Kelly H <kellyaccnts@gmail.com>
2020-01-20 12:29:54 -0700

    minor changes to dll tests, add context to main app test

--- react-02/src/App.test.js
+++ react-02/src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Context, ThemeContextProvider } from "./ThemeContextProvider";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+        <ThemeContextProvider value={{ fontSize: 11 }}>
+            <App />
+        </ThemeContextProvider>,
+        div
+    );
+    ReactDOM.unmountComponentAtNode(div);
 });
--- react-02/src/components/linkedList/model/DoublyLinkedList.test.js
+++ react-02/src/components/linkedList/model/DoublyLinkedList.test.js
@@ -8,21 +8,18 @@ import { ListNode, DoublyLinkedList } from "./DoublyLinkedList";
 // });
 
 test("insert", () => {
-    let ll = new DoublyLinkedList();
-    ll.insert("A", 1);
+    let ll = new DoublyLinkedList("A", 1);
     expect(ll.showNodes()).toBe("subject: A amount: 1 <-> ");
     ll.insert("B", 1);
     expect(ll.showNodes()).toBe(
         "subject: A amount: 1 <-> subject: B amount: 1 <-> "
     );
-    ll.prev();
+    ll.previous();
     ll.insert("C", 1);
     expect(ll.showNodes()).toBe(
         "subject: A amount: 1 <-> subject: C amount: 1 <-> subject: B amount: 1 <-> "
     );
-    ll.prev();
-    ll.prev();
-    ll.prev();
+    ll.next();
     ll.insert("D", 2);
     expect(ll.showNodes()).toBe(
         "subject: A amount: 1 <-> subject: C amount: 1 <-> subject: B amount: 1 <-> subject: D amount: 2 <-> "
@@ -30,36 +27,39 @@ test("insert", () => {
 });
 
 test("First and last", () => {
-    let ll = new DoublyLinkedList();
-    expect(ll.first()).toBe(null);
-    expect(ll.last()).toBe(null);
-    ll.insert("A", 1);
-    ll.insert("B", 2);
+    let ll = new DoublyLinkedList("A", 0);
     expect(ll.first().subject).toBe("A");
-    expect(ll.last().subject).toBe("B");
+    expect(ll.last().subject).toBe("A");
+    ll.insert("B", 1);
+    ll.insert("C", 2);
+    expect(ll.first().subject).toBe("A");
+    expect(ll.last().subject).toBe("C");
 });
 
-test("next and prev", () => {
-    let ll = new DoublyLinkedList();
+test("next and previous", () => {
+    let ll = new DoublyLinkedList("HEAD", 0);
     ll.insert("A", 1);
     ll.insert("B", 2);
-    expect(ll.prev().subject).toBe("A");
+    expect(ll.previous().subject).toBe("A");
     expect(ll.next().subject).toBe("B");
 });
 
 test("deletion", () => {
-    let ll = new DoublyLinkedList();
+    let ll = new DoublyLinkedList("HEAD", 0);
     ll.insert("A", 1);
     ll.insert("B", 2);
-    ll.prev();
+    ll.previous();
     ll.delete();
-    expect(ll.showNodes()).toBe("subject: B amount: 2 <-> ");
+    expect(ll.showNodes()).toBe(
+        "subject: HEAD amount: 0 <-> subject: B amount: 2 <-> "
+    );
+    ll.delete();
     ll.delete();
     expect(ll.showNodes()).toBe("");
 });
 
 test("sum node amounts", () => {
-    let ll = new DoublyLinkedList();
+    let ll = new DoublyLinkedList("HEAD", 0);
     ll.insert("A", 1);
     ll.insert("B", 2);
     ll.insert("C", 4);
@@ -68,22 +68,22 @@ test("sum node amounts", () => {
 });
 
 test("clone function", () => {
-    let ll = new DoublyLinkedList();
+    let ll = new DoublyLinkedList("HEAD", 0);
     ll.insert("A", 1);
     ll.insert("B", 2);
     ll.insert("C", 4);
     const clone = ll.clone();
     expect(clone.showNodes()).toBe(
-        "subject: A amount: 1 <-> subject: B amount: 2 <-> subject: C amount: 4 <-> "
+        "subject: HEAD amount: 0 <-> subject: A amount: 1 <-> subject: B amount: 2 <-> subject: C amount: 4 <-> "
     );
 });
 
 test("map function", () => {
-    let ll = new DoublyLinkedList();
+    let ll = new DoublyLinkedList("HEAD", 0);
     ll.insert("A", 1);
     ll.insert("B", 2);
     ll.insert("C", 4);
     ll.insert("D", 8);
     let mapTest = ll.map(x => x.amount * 2);
-    expect(mapTest).toStrictEqual([2, 4, 8, 16]);
+    expect(mapTest).toStrictEqual([0, 2, 4, 8, 16]);
 });
