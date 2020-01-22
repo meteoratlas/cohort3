8664ffe3114d6b27c43c75fb0e3963550b78d0ff
Kelly H <kellyaccnts@gmail.com>
2020-01-21 22:10:17 -0700

    basic invoice printing

--- src/python/230/create_invoice.py
+++ src/python/230/create_invoice.py
@@ -1,4 +1,5 @@
 from openpyxl import Workbook, load_workbook
+from datetime import datetime
 
 def read_workbook(file):
     book = load_workbook(file)
@@ -22,15 +23,33 @@ def get_row_by_id(id, column):
     return row
 
 def create_invoice(data):
-    # requested = input("Please enter an invoice id to generate (or any other value to quit): ")
+    requested = input("Please enter an invoice id to generate (or any other value to quit): ")
     [customers, products, invoice_items, invoices] = data
-    requested = 5
     try:
         requested = int(requested)
     except:
         print("Invalid input, quitting.")
     if requested < 0:
         print("Invalid input, quitting.")
+
+    inv = invoices[requested]
+    c_info = customers[inv["customer_id"]]
+    inv_item_info = invoice_items[inv["invoice_item_id"]]
+    product_info = products[inv_item_info["product_id"]]
+
+    width = 80
+    report =  "=" * width + "\n"
+    report += " INVOICE ".center(width, "=") + "\n"
+    report += "=" * width + "\n"
+
+    report += f'{c_info["first_name"]} {c_info["last_name"]}\nCustomer ID # {c_info["customer_id"]}\n{c_info["phone_number"]}\n{c_info["email"]}\n'
+    report += f'{ ( inv["date"]).strftime("%d/%m/%y")}\n'
+    report += "-" * width + "\n"
+    report += f'{product_info["name"]}\tQuantity: {inv_item_info["quantity"]}\n'
+    report += f'Total cost: ${inv["total_cost"]}\n'
+
+    with open("Invoice.txt", "w") as txt:
+        txt.write(report)
     
 data = read_workbook("data.xlsx")
 create_invoice(data)
