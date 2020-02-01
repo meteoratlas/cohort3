09bba05005ab7192f5519ae1a61d0374d277193f
Kelly H <kellyaccnts@gmail.com>
2020-01-31 23:14:12 -0700

    dict to json, fixed small data error

--- src/python/230/create_invoice.py
+++ src/python/230/create_invoice.py
@@ -1,9 +1,10 @@
 from openpyxl import Workbook, load_workbook
-from datetime import datetime
+import datetime
+import json
 
 def read_workbook(file):
     book = load_workbook(file)
-    sheet_dicts = []
+    sheet_dicts = {}
     for sheet in book:
         data = {}
         titles = [t.value for t in sheet[1]]
@@ -13,14 +14,12 @@ def read_workbook(file):
                 continue
             row_data = {}
             for j, info in enumerate(row):
+                if (j == 0): continue
+                # print(info)
                 row_data[titles[j]] = info
             data[row_data[titles[0]]] = row_data
-        sheet_dicts.append(data)
+        sheet_dicts[sheet.title] = data
     return sheet_dicts
-
-def get_row_by_id(id, column):
-    [row] = filter(lambda x:x==id, column)
-    return row
 
 def create_invoice(data):
     requested = input("Please enter an invoice id to generate (or any other value to quit): ")
@@ -50,6 +49,16 @@ def create_invoice(data):
 
     with open("Invoice.txt", "w") as txt:
         txt.write(report)
-    
-data = read_workbook("data.xlsx")
-create_invoice(data)
+
+def makeJSON(data):
+    def dateToString(date):
+        if isinstance(date, datetime.datetime):
+            return date.__str__()
+
+    with open("info.json", "w") as txt:
+        txt.write(json.dumps(data, default = dateToString, indent=4, separators=(',', ': ')))
+
+if __name__ == "__main__":
+    data = read_workbook("data.xlsx")
+    # makeJSON(data)
+    # create_invoice(data)
