cc1a67403c3fa8362928e91ce2c6b79db98376d4
Kelly H <kellyaccnts@gmail.com>
2020-02-19 10:42:31 -0700

    240: react front end baseline

--- src/python/240/api-front-end/src/App.js
+++ src/python/240/api-front-end/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        console.log("on mount");
+    }, []);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>React Frontend</h1>
+            </header>
+            <main>
+                <h2>Returned JSON</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
