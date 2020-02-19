3aec63a795713c6b7d428c660c8f3c07c7ad48a5
Kelly H <kellyaccnts@gmail.com>
2020-02-19 11:36:23 -0700

    complete 240

--- src/python/240/api-front-end/src/App.js
+++ src/python/240/api-front-end/src/App.js
@@ -4,6 +4,11 @@ import "./App.css";
 
 function App() {
     const [data, setData] = useState({});
+    const [showDataTypes, setShowDataTypes] = useState(true);
+    const [serverResponseMsg, setServerResponseMsg] = useState("Connecting...");
+    setTimeout(() => {
+        setServerResponseMsg("Could not connect to server.");
+    }, 5000);
     useEffect(() => {
         fetch("http://127.0.0.1:5000/getall")
             .then(response => {
@@ -18,14 +23,37 @@ function App() {
             })
             .catch(error => {});
     }, []);
+    const handleInputChange = e => {
+        const target = e.target;
+        const value =
+            target.type === "checkbox" ? target.checked : target.value;
+        setShowDataTypes(value);
+    };
     return (
         <div className="App">
             <header className="App-header">
                 <h1>React Frontend</h1>
+                <form>
+                    <label>Show Data Types in JSON?</label>
+                    <input
+                        type="checkbox"
+                        checked={showDataTypes}
+                        onChange={handleInputChange}
+                    ></input>
+                </form>
             </header>
             <main>
-                <h2>Returned JSON</h2>
-                <ReactJson src={data} theme="ocean" enableClipboard={false} />
+                {Object.entries(data).length === 0 &&
+                data.constructor === Object ? (
+                    <h2>{serverResponseMsg}</h2>
+                ) : (
+                    <ReactJson
+                        src={data}
+                        theme="twilight"
+                        enableClipboard={false}
+                        displayDataTypes={showDataTypes}
+                    />
+                )}
             </main>
         </div>
     );
