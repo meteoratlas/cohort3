d5d68c528dec4a8122b0e45dd24eed840bafd922
Kelly H <kellyaccnts@gmail.com>
2020-02-29 23:36:41 -0700

    api add sqlalchemy base, hook up react template

--- src/python/250/api/api.py
+++ src/python/250/api/api.py
@@ -1,10 +1,28 @@
 import time
-from flask import Flask
+from flask import Flask, render_template
+from flask_restful import Api
+import keys
+from database import db
 
-app = Flask(__name__)
+
+# TODO: make sure this works in production
+app = Flask(__name__, static_folder="../build/static", template_folder="../build")
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = keys.secret_key
+# TODO: double check this works when deployed in production
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
 def home():
-    return {'serverActive':True, 'time': time.time()}
+    return render_template('index.html')
 
-app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    db.init_app(app)
+    app.run(port=5000, debug=True)
