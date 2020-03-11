3c9d61376b7f58a447b2e5a14b614152c066c494
Kelly H <kellyaccnts@gmail.com>
2020-03-11 09:35:04 -0600

    remove draft-js dependency, add fetch

--- src/python/250/src/App.js
+++ src/python/250/src/App.js
@@ -4,6 +4,28 @@ import Button from "react-bootstrap/Button";
 import PostEditor from "./components/PostEditor";
 
 function App() {
+    function fetch(operation) {
+        return `${window.location.href}${operation}`;
+    }
+    async function postData(url, data = {}) {
+        const response = await fetch(url, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            redirect: "follow",
+            referrer: "no-referrer",
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+
+        const json = await response.json(); // parses JSON response into native JavaScript objects
+        json.status = response.status;
+        json.statusText = response.statusText;
+        return json;
+    }
     return (
         <div className="App">
             <header>
